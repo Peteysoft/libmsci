2825f35fe77ca805d693baaf31513fef867750d1
Petey <petey@peteysoft.org>
2016-04-03 18:50:20 -0400

    Working on SVM acceleration paper

--- libagf/scripts/mb_nb.sh
+++ libagf/scripts/mb_nb.sh
@@ -7,17 +7,22 @@ set -xe
 NTRIAL=20
 NTEST=10
 MAXSAMPLE=50
+FRAC=0.2
 
-while getopts 'gMN:q:s:' DUM
+while getopts 'gKMN:f:q:s:' DUM
 do
   case $DUM in
     g) LOGFLAG=1
+      ;;
+    K) KFLAG=1
       ;;
     M) SVMFLAG=1
       TRAIN_COMMAND=svm_accelerate
       CLASSIFY_COMMAND=classify_c
       ;;
     N) NTRIAL=$OPTARG
+      ;;
+    f) FRAC=$OPTARG
       ;;
     q) NTEST=$OPTARG
       ;;
@@ -50,52 +55,62 @@ elif [ $# -eq 2 ]; then
     echo $TRAIN_COMMAND
   fi
 else
-  echo "script.sh [-g] [-M] [-N ntrial] [-q ntest] [-s maxsample]\\"
-  echo "        [model] train output"
+  echo "   MULTI-BORDERS NUMBER OF BORDERS"
+  echo
+  echo "mb_nb.sh [-g] [-M] [-K] [-N ntrial] [-q ntest] [-s maxsample]\\"
+  echo "        [-f frac] [model] train output"
   echo
   echo "  model     = LIBSVM model or multi-borders control file"
   echo "  train     = training data"
   echo "  output    = output skill scores"
   echo
   echo "  g         = use logarithmic progression"
+  echo "  K         = keep temporary files"
   echo "  M         = use LIBSVM model"
-  echo "  ntrial    = number of repeated trials"
-  echo "  ntest     = number of sample sizes"
-  echo "  maxsample = maximum number of border samples"
+  echo
+  echo "  ntrial    = number of repeated trials [$NTRIAL]"
+  echo "  ntest     = number of sample sizes [$NTEST]"
+  echo "  maxsample = maximum number of border samples [$MAXSAMPLE]"
+  echo "  frac      = fraction to reserve for testing [$FRAC]"
   exit 0
 fi
 
-MODEL=dum.agf
-TEST=testdata
-#TEST_POINTS=${TEST}.vec
-OUTPUT=dum
-#TEST_CLASSES=${TEST}.cls
-TRAIN="$CONTROL $TRAINING_DATA"
-
+BASE=mb$RANDOM.tmp
+MODEL=$BASE.agf
+OUTPUT=$BASE
 
 rm -f ${OUTFILE}
 rm -f train.log
 rm -f test.log
 
+
 for ((I=0; I<NTRIAL; I++)); do
-  /home/lenovo/my_software/libmsci/libagf/examples/sample_classes/sample_class -R 1000 2000 ${TEST}$I > stuff.txt
+  #/home/lenovo/my_software/libmsci/libagf/examples/sample_classes/sample_class -R 1000 2000 ${TEST}$I > stuff.txt
+  agf_preprocess -zf $FRAC ${TRAINING_DATA} $BASE.$I.trn $BASE.$I.tst
 done
 
 for ((I=0; I<NTEST; I++)); do
   if [ $LOGFLAG ]; then
-    n=$(echo "scale=2; e(($I+1)*l($MAXSAMPLE)/$NTEST)" | bc -l)
+    n=$(echo "e(($I+1)*l($MAXSAMPLE)/$NTEST)" | bc -l)
   else
     n=$(((I+1)*MAXSAMPLE/NTEST))
   fi
-  echo $NTRIAL
+
   for ((J=0; J<NTRIAL; J++)); do
+    TRAIN="$CONTROL $BASE.$J.trn"
+    TEST="$BASE.$J.tst"
     echo "(time ${TRAIN_COMMAND} -s $n ${TRAIN} ${MODEL}) 2>> train.log"
     (time ${TRAIN_COMMAND} -s $n ${TRAIN} ${MODEL}) 2>> train.log
-    (time ${CLASSIFY_COMMAND} ${MODEL} ${TEST}$I.vec ${OUTPUT} > junk.txt ) 2>> test.log
+    echo "(time ${CLASSIFY_COMMAND} ${MODEL} ${TEST}.vec ${OUTPUT} > junk.txt ) 2>> test.log"
+    (time ${CLASSIFY_COMMAND} ${MODEL} ${TEST}.vec ${OUTPUT} > junk.txt ) 2>> test.log
     echo -n "$n " >> ${OUTFILE}
-    cls_comp_stats -Hb ${TEST}$I.cls ${OUTPUT} >> ${OUTFILE}
+    cls_comp_stats -Hb ${TEST}.cls ${OUTPUT} >> ${OUTFILE}
   done
   echo $NTRIAL
 done
 
+#clean up, remove temporary files:
+if [ -z $KFLAG ]; then
+  rm -f $BASE.*
+fi
 
---
+++ libagf/scripts/svm_nsv.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+#BASE=/home/lenovo/my_software/libmsci/libagf/examples/sample_classes
+
+N="10 20 30 40 50 75 100 200 300 400 500 750 1000 2000 3000 4000 5000 7500 10000"
+#N="10 50 100 500 1000 5000 10000"
+
+#$BASE/sample_class -R 1000 2000 sctest > sctest.lvq
+#agf2ascii -M sctest sctest.svm
+
+#numbers:
+NTRIAL=1
+NTEST=10
+MAXSAMPLE=100
+
+while getopts 'GKMq:s:c:d:g:h:r:t:' DUM
+do
+  case $DUM in
+    G) LOGFLAG=1
+      ;;
+    K) KFLAG=1
+      ;;
+    M) SVMFLAG=1
+      ;;
+    N) NTRIAL=$OPTARG
+      ;;
+    q) NTEST=$OPTARG
+      ;;
+    s) MAXSAMPLE=$OPTARG
+      ;;
+    c) SVMOPT="$SVMOPT -c $OPTARG"
+      ;;
+    d) SVMOPT="$SVMOPT -d $OPTARG"
+      ;;
+    g) SVMOPT="$SVMOPT -g $OPTARG"
+      ;;
+    h) SVMOPT="$SVMOPT -h $OPTARG"
+      ;;
+    r) SVMOPT="$SVMOPT -r $OPTARG"
+      ;;
+    t) SVMOPT="$SVMOPT -t $OPTARG"
+      ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+
+#output file:
+OUTFILE=stats.txt
+
+echo $#
+
+if [ $# -eq 2 ]; then
+  TRAINING_DATA=$1
+  OUTFILE=$2
+  if [ -z $SVMFLAG ]; then
+    TRAIN_COMMAND=multi_borders
+    CLASSIFY_COMMAND=classify_m
+  fi
+elif [ $# -eq 2 ]; then
+  TRAINING_DATA=$1
+  OUTFILE=$2
+  if [ -z $SVMFLAG ]; then
+    TRAIN_COMMAND=class_borders
+    CLASSIFY_COMMAND=classify_b
+    echo $TRAIN_COMMAND
+  fi
+else
+  echo "   SUPPORT-VECTOR-MACHINE NUMBER OF SUPPORT VECTORS"
+  echo
+  echo "mb_nb.sh [-g] [-M] [-K] [-N ntrial] [-q ntest]\\"
+  echo "        [options] train output"
+  echo
+  echo "  options   = options to pass to svm-train"
+  echo "  train     = training data"
+  echo "  output    = output stats"
+  echo
+  echo "  G         = use logarithmic progression"
+  echo "  K         = keep temporary files"
+  echo "  M         = data is in LIBSVM format"
+  echo
+  echo "  ntrial    = number of repeated trials [$NTRIAL]"
+  echo "  ntest     = number of sample sizes [$NTEST]"
+  echo "  frac      = fraction to reserve for testing [$FRAC]"
+  exit 0
+fi
+
+BASE=sv$RANDOM.tmp
+
+if test $SVMFLAG; then
+  #this is kind of dum:
+  svm2agf ${TRAINING_DATA} $BASE.0
+  NTRAIN=$(wc -l < ${TRAINING_DATA})
+  TRAINING_DATA=$BASE.0
+else
+  agf2ascii -M ${TRAINING_DATA} $BASE.0.svm
+  NTRAIN=$(wc -l < $BASE.0.svm)
+fi
+
+
+rm -f $OUTFILE
+
+for ((I=0; I<NTEST; I++)); do
+  if [ $LOGFLAG ]; then
+    n=$(echo "e(($I+1)*l($MAXSAMPLE)/$NTEST)" | bc -l)
+  else
+    n=$(((I+1)*MAXSAMPLE/NTEST))
+  fi
+
+  f=$(echo "$n./$MAXSAMPLE" | bc -l)
+
+  #$BASE/sample_class $n $((n*2)) sctrain > sctrain.lvq
+  #agf2ascii -M sctrain sctrain.svm
+
+  agf_preprocess -zf $f ${TRAINING_DATA} $BASE.trn $BASE.tst
+  agf2ascii -M $BASE.trn $BASE.trn.svm
+  agf2ascii -M $BASE.tst $BASE.tst.svm
+
+  svm-train $SVMOPT $BASE.tst.svm $BASE.svmmod
+  svm-predict $BASE.trn.svm $BASE.svmmod $BASE.svmout
+  echo -n "$(wc -l < $BASE.tst.svm) " >> $OUTFILE
+  grep nr_sv $BASE.svmmod >> $OUTFILE
+done
+
+#rm -f $BASE.*
+  
