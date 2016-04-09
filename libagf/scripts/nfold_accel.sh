766441e5eac36ecd98fea38c4dfdad63f33f2ecd
Petey <petey@peteysoft.org>
2016-04-09 03:10:29 -0400

    Should just throw everything into one general purpose script and then convert it to a makefile...

--- libagf/scripts/nfold_accel.sh
+++ libagf/scripts/nfold_accel.sh
@@ -45,23 +45,29 @@ done
 
 shift $((OPTIND - 1))
 
-#output file:
-OUTFILE=stats.txt
+#base name for intermediate files:
+BASE=accel$RANDOM.tmp
 
 echo $#
 
 if [ $# -eq 2 ]; then
   CONTROL=$1
   TRAINING_DATA=$2
+  RESULTBASE=$BASE.results
+elif [ $# -eq 3 ]; then
+  CONTROL=$1
+  TRAINING_DATA=$2
+  RESULTBASE=$3
 else
   echo "   N-FOLD FOR ACCELERATED SVM"
   echo
   echo "nfold_accel.sh [-M] [-K] [-s nsamples] [-d ntrial] [-Q method]\\"
-  echo "           [svm-options] control train"
+  echo "           [svm-options] control train out"
   echo
   echo "  svm-options  = options to pass to svm-train"
   echo "  control    = input control file"
   echo "  train     = training data"
+  echo "  out       = base name for output files"
   echo
   echo "  K         = keep temporary files"
   echo "  M         = data is in LIBSVM format"
@@ -72,7 +78,6 @@ else
   exit 0
 fi
 
-BASE=accel$RANDOM.tmp
 
 if test $SVMFLAG; then
   #this is kind of dum:
@@ -103,43 +108,73 @@ for ((I=0; I<NTRIAL; I++)); do
   svm2agf $BASE.trn-$ISTRING.svm $BASE.trn-$ISTRING
 done
 
+rm -f svm_train.log
+rm -f svm_test.log
+
+rm -f border_train.log
+rm -f border_test.log
+
 for ((I=0; I<NTRIAL; I++)); do
   ISTRING=$(printf "%2.2d" $I)
+  TRAINBASE=$BASE.trn-$ISTRING
+  TESTBASE=$BASE.tst-$ISTRING
+  OUTBASE=$BASE.$ISTRING
 
-  echo "multi_borders -M -- \"svm-train -b 1 $SVMOPT\" $CONTROL $BASE.trn-$ISTRING.svm $BASE $BASE.svc"
-  multi_borders -M -- "svm-train -b 1 $SVMOPT" $CONTROL $BASE.trn-$ISTRING.svm $BASE $BASE.svc
-  echo "classify_m -M -O \"svm-predict -b 1\" -Q $METHOD $BASE.svc $BASE.tst-$ISTRING.svm $BASE.$ISTRING.svm >> $BASE.txt"
-  classify_m -M -O "svm-predict -b 1" -Q $METHOD $BASE.svc $BASE.tst-$ISTRING.svm $BASE.$ISTRING.svmout >> $BASE.txt
-  echo "svmout2agf $BASE.$ISTRING.svmout $BASE.svm.$ISTRING >> $BASE.txt"
-  svmout2agf $BASE.$ISTRING.svmout $BASE.svm.$ISTRING >> $BASE.txt
+  #Step 1: make a fully native LIBSVM model to:
+  #	a) check the time
+  #	b) compare against hybrid model
+  echo "(time svm-train -b 1 $SVMOPT $TRAINBASE.svm $OUTBASE.svmmod) 2>> svm_train.log"
+  (time svm-train -b 1 $SVMOPT $TRAINBASE.svm $OUTBASE.svmmod) 2>> svm_train.log
+  echo "(time svm-predict -b 1 $TESTBASE.svm $OUTBASE.svmmod $OUTBASE.0.svmout) 2>> svm_test.log"
+  (time svm-predict -b 1 $TESTBASE.svm $OUTBASE.svmmod $OUTBASE.0.svmout) 2>> svm_test.log
+  echo "svmout2agf $OUTBASE.0.svmout $BASE.libsvm.$ISTRING >> $BASE.junk.txt"
+  svmout2agf $OUTBASE.0.svmout $BASE.libsvm.$ISTRING >> $BASE.junk.txt
 
-  echo "multi_borders -Z -s $NSAMPLE $BASE.svc $BASE.trn-$ISTRING $BASE $BASE.mbc"
-  multi_borders -Z -s $NSAMPLE $BASE.svc $BASE.trn-$ISTRING $BASE $BASE.mbc
-  echo "classify_m -Q $METHOD $BASE.mbc $BASE.tst-$ISTRING.vec $BASE.$ISTRING >> $BASE.txt"
-  classify_m -Q $METHOD $BASE.mbc $BASE.tst-$ISTRING.vec $BASE.$ISTRING >> $BASE.txt
+  #Step 2: build the hybrid borders/libsvm model:
+  echo "multi_borders -M -- \"svm-train -b 1 $SVMOPT\" $CONTROL $TRAINBASE.svm $BASE $OUTBASE.svc"
+  multi_borders -M -- "svm-train -b 1 $SVMOPT" $CONTROL $TRAINBASE.svm $OUTBASE $OUTBASE.svc
+  echo "classify_m -M -O \"svm-predict -b 1\" -Q $METHOD $OUTBASE.svc $TESTBASE.svm $OUTBASE.svm >> $BASE.txt"
+  classify_m -M -O "svm-predict -b 1" -Q $METHOD $OUTBASE.svc $TESTBASE.svm $OUTBASE.svmout >> $BASE.txt
+  echo "svmout2agf $OUTBASE.svmout $BASE.svm.$ISTRING >> $BASE.txt"
+  svmout2agf $OUTBASE.svmout $BASE.svm.$ISTRING >> $BASE.txt
+
+  #Step 3: accelerate the hybrid model:
+  echo "(time (multi_borders -Z -s $NSAMPLE $OUTBASE.svc $TRAINBASE $OUTBASE $OUTBASE.mbc 2>> $BASE.jnk)) 2>> border_train.log"
+  (time (multi_borders -Z -s $NSAMPLE $OUTBASE.svc $TRAINBASE $OUTBASE $OUTBASE.mbc 2>> $BASE.jnk)) 2>> border_train.log
+  echo "(time (classify_m -Q $METHOD $OUTBASE.mbc $TESTBASE.vec $OUTBASE &>> $BASE.txt)) 2>> border_test.log"
+  (time (classify_m -Q $METHOD $OUTBASE.mbc $TESTBASE.vec $OUTBASE &>> $BASE.txt)) 2>> border_test.log
 done
 
-echo "cat $BASE.svm.??.cls > $BASE.svm.cls"
-cat $BASE.svm.??.cls > $BASE.svm.cls
-echo "cat $BASE.svm.??.con > $BASE.svm.con"
-cat $BASE.svm.??.con > $BASE.svm.con
+echo "cat $BASE.libsvm.??.cls > $RESULTBASE.libsvm.cls"
+cat $BASE.libsvm.??.cls > $RESULTBASE.libsvm.cls
+echo "cat $BASE.libsvm.??.con > $RESULTBASE.libsvm.con"
+cat $BASE.libsvm.??.con > $RESULTBASE.libsvm.con
 
-echo "cat $BASE.??.cls > $BASE.out.cls"
-cat $BASE.??.cls > $BASE.out.cls
-echo "cat $BASE.??.con > $BASE.out.con"
-cat $BASE.??.con > $BASE.out.con
+echo "cat $BASE.svm.??.cls > $RESULTBASE.svm.cls"
+cat $BASE.svm.??.cls > $RESULTBASE.svm.cls
+echo "cat $BASE.svm.??.con > $RESULTBASE.svm.con"
+cat $BASE.svm.??.con > $RESULTBASE.svm.con
 
-cat $BASE.tst-??.cls > $BASE.cls
+echo "cat $BASE.??.cls > $RESULTBASE.cls"
+cat $BASE.??.cls > $RESULTBASE.out.cls
+echo "cat $BASE.??.con > $RESULTBASE.con"
+cat $BASE.??.con > $RESULTBASE.con
 
-echo "cls_comp_stats $BASE.cls $BASE.svm"
-cls_comp_stats $BASE.cls $BASE.svm
+cat $BASE.tst-??.cls > $RESULTBASE.truth.cls
 
-echo "cls_comp_stats $BASE.cls $BASE.out"
-cls_comp_stats $BASE.cls $BASE.out
+echo "cls_comp_stats $RESULTBASE.libsvm.cls $RESULTBASE.svm"
+cls_comp_stats $RESULTBASE.libsvm.cls $RESULTBASE.svm
+agf_correlate $RESULTBASE.libsvm.con $RESULTBASE.svm.con
+
+echo "cls_comp_stats truth.$BASE.cls $RESULTBASE.svm"
+cls_comp_stats truth.$BASE.cls $RESULTBASE.svm
+
+echo "cls_comp_stats truth.$BASE.cls $RESULTBASE.out"
+cls_comp_stats truth.$BASE.cls $RESULTBASE.out
 
 #clean up, remove temporary files:
 if [ -z $KFLAG ]; then
   echo "rm -f $BASE*"
-  #rm -f $BASE*
+  rm -f $BASE*
 fi
   
