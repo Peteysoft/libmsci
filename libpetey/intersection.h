add9a742bce4f4b846a832d390f4ce9cdd3688d5
Petey <petey@peteysoft.org>
2017-09-29 23:54:22 -0400

    Getting ready to test ortho coding matrix: - unify binary LIBSVM support vectors - use unified set for quicker classification: "memoization"

---
+++ libpetey/intersection.h
@@ -0,0 +1,25 @@
+
+namespace libpetey {
+
+  template <typename scalar>
+  int compare_vectors(scalar *v1, scalar *v2, int n);
+
+  template <typename scalar>
+  int intersect_vectors(scalar ***v, 		//list of lists of vectors
+		  int *n, 			//number of elements in each list
+		  int n0, 			//number of lists
+		  int D,			//dimension of each vector
+		  scalar **result,		//intersection
+		  int **ind);			//indexes into result
+
+  template <typename tp>
+  int32_t intersection(tp *l1, int32_t n1, tp *l2, int32_t n2, tp *lr);
+
+  //returns 0 for success:
+  template <typename scalar>
+  int test_intersect_vectors(int nv, 		//total number of vectors(union)
+		  int D, 			//dimension of each vector
+		  int nss, 			//number of sub sets
+		  float frac);			//fraction for each ss
+
+}
